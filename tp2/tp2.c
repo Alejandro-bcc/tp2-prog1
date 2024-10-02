1f4d19376181f706124f7de837debf892af21e5e
Alejandro <alejandro.nava@ufpr.br>
2024-10-01 21:58:11 -0300

    Files

---
+++ tp2/tp2.c
@@ -0,0 +1,25 @@
+/* 
+ * Tipos Abstratos de Dados - TADs
+ * Arquivo do programa principal, que usa o TAD racional.
+ * Feito em 24/09/2024 para a disciplina CI1001 - Programação 1,
+ * pelo aluno Alejandro David Nava Nava.
+ * GRR 20242778.
+*/
+
+/* coloque aqui seus includes (primeiro os <...>, depois os "...") */
+#include <stdio.h>
+#include "racional.h"
+
+/* coloque aqui as funções auxiliares que precisar neste arquivo */
+
+/* programa principal */
+int main ()
+{
+	struct racional vetor[100];
+
+	int n, i;
+
+	scanf("&d",&n);
+
+  return (0) ;
+}
